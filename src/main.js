7a0f8fb1df3c4d4dcb4c7ccc449522036461a626
HelloYLee <liyu13857341794@163.com>
2023-03-09 14:03:48 +0800

    add eslint

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import count from "./js/count";
 import sum from "./js/multisum";
+import './css/iconfont.css'
 import './css/common.css'
 import './less/common.less'
 import './sass/common.sass'
 import './sass/common.scss'
 import './stylus/common.styl'
 
-const countNum = count(1, 111)
+let countNum = count(1, 111)
 console.log(countNum)
 const multiSum = sum(1,99,3,32)
 console.log(multiSum)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const EslintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
     // 入口 相对路径
@@ -6,7 +7,8 @@ module.exports = {
     // 输出
     output: {
        path: path.resolve(__dirname, "dist"),
-       filename: 'main.js'
+       filename: 'static/js/main.js',
+       clean: true, // 打包前清空之前的内容
     },
     // loader
     module: {
@@ -30,11 +32,28 @@ module.exports = {
                     maxSize: 50 * 1024  // 配置为小于50KB转换为data URL
                 }
             } */
+            generator: {
+                filename: 'static/images/[hash:10][ext][query]'  //哈希值只取前十位
+            }
+        }, {
+            test: /\.(ttf|woff2?)$/,
+            type: 'asset/resource', // 不转换base64格式)
+/*             parser: {
+                dataUrlCondition: {
+                    maxSize: 50 * 1024  // 配置为小于50KB转换为data URL
+                }
+            } */
+            generator: {
+                filename: 'static/media/[hash:10][ext][query]'
+            }
 
         }]
     },
     // plugins
-    plugins: [],
+    plugins: [new EslintPlugin({
+        // 指定哪些文件目录需要做eslint检查
+        context: path.resolve(__dirname, "src")
+    })],
 
     // mode
     mode: 'development'
